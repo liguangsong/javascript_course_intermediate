951f5461a58e75d1324dd18e39e84fd54c784fe4
Channely <qijie29896@gmail.com>
2014-09-09 14:17:41 +0800

    debug

--- codes/specs/section_0/challenge_0_2.js
+++ codes/specs/section_0/challenge_0_2.js
@@ -4,11 +4,11 @@ describe('section_0', function () {
 //        done();
 //    });
 
-    it("challenge_0_2", function (done) {
+    it("challenge_0_2", function () {
         expect(typeof logs[0]).toBe('string');
         expect(logs[0]).toBe("数组中含有奇数19个,偶数11个.");
         var data = [1,2];
-//        expect(analyse_array()).toBe("数组中含有奇数1个,偶数1个.");
+        expect(analyse_array()).toBe("数组中含有奇数1个,偶数1个.");
 
 //        done();
     });
