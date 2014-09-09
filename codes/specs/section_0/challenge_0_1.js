a7d2c830bad6d593c81f23e8850930fb3e1fcf48
Channely <qijie29896@gmail.com>
2014-09-09 14:32:59 +0800

    update to new version success

--- codes/specs/section_0/challenge_0_1.js
+++ codes/specs/section_0/challenge_0_1.js
@@ -5,7 +5,7 @@ describe('section_0', function () {
         expect(logs[0]).toBe("数组中含有奇数19个,偶数11个.");
         data = [1,2];
         expect(analyse_array()).toBe("数组中含有奇数1个,偶数1个.");
-        
+
     });
 });
 
