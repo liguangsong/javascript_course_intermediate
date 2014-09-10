cb36c093598df577379b0c144ac5c1d8c551dc1b
Channely <qijie29896@gmail.com>
2014-09-10 20:37:00 +0800

    add code for practice 2 2

--- codes/specs/section_2/challenge_2_1.js
+++ codes/specs/section_2/challenge_2_1.js
@@ -6,6 +6,6 @@ describe('section_2', function () {
         expect(cycling.usage).toBe("transport");
         expect(typeof logs[0]).toBe('string');
         expect(logs[0]).toBe("This is cycling, the usage of it is transport.");
-        
+
     });
 });
--- codes/specs/section_2/challenge_2_2.js
+++ codes/specs/section_2/challenge_2_2.js
@@ -2,7 +2,11 @@ describe('section_2', function () {
 
     it("challenge_2_2", function () {
 
-        expect(true).toBe(true);
+        expect(cycling === undefined).toBeFalsy();
+        expect(cycling.name).toBe("cycling");
+        expect(cycling.usage).toBe("transport");
+        expect(typeof logs[0]).toBe('string');
+        expect(logs[0]).toBe("cycling,transport,1970");
 
     });
 });
