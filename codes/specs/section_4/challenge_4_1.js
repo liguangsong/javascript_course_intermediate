00f9337e3733bc6934a4396d31b5b9d961f83f6c
Channely <qijie29896@gmail.com>
2014-09-15 15:49:40 +0800

    add new practice and section

---
+++ codes/specs/section_4/challenge_4_1.js
@@ -0,0 +1,7 @@
+describe('section_4', function () {
+    it("challenge_4_1", function () {
+
+        expect(true).toBe(true);
+
+    });
+});
---
+++ codes/specs/section_4/challenge_4_2.js
@@ -0,0 +1,8 @@
+describe('section_4', function () {
+
+    it("challenge_4_2", function () {
+
+        expect(true).toBe(true);
+
+    });
+});
