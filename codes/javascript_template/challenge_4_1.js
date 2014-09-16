e16c5760b1b1ba4606f7a84e894b9c3917a3abf7
Channely <qijie29896@gmail.com>
2014-09-16 12:25:56 +0800

    add practice

--- codes/javascript_template/challenge_4_1.js
+++ codes/javascript_template/challenge_4_1.js
@@ -1,1 +1,29 @@
-demo
+//全局变量
+var info = [];
+
+//普通的show函数，将数据的内容打印到控制台
+function show(data){
+    info.push(data)
+    //如果是可以直接输出的字符串则直接输出
+    if ( typeof data === "string")
+    {
+        console.log(data);
+    }
+    else if ( typeof data === "object"){
+        //遍历data
+        for(var item in data){
+            console.log(item + ": " + data[item]);
+        }
+    }
+}
+
+//定义一个接收两个参数的函数，参数中后面一个是回调函数, 在函数体中调用回调函数,并将第一个参数传入回调函数.
+
+
+
+
+//当我们调用get_inputs函数时，我们将show函数作为一个参数传递给它     
+//因此show将会在get_inputs函数内被回调（或者执行）     
+get_inputs({name:"bower",speciality:"Robot"}, show);
+//name:bower
+//speciality:Robot
--- section_4/practice_1_desc.md
+++ section_4/practice_1_desc.md
@@ -1,1 +1,8 @@
-demo
+当你使用类似于function\_1(function\_2)方法的时候函数function\_2就成了回调函数，而你还可以使用function\_1(function\_3)这个时候，函数function\_3就成了回调函数。如果你写成了
+
+    function function\_1(
+        .....;
+        function\_2()
+    )
+
+就失去了变量的灵活性。所以回调函数的使用可以给我们的代码带来极大的灵活性,而良好的灵活性会给编码人员带来更有想象力的空间去发挥创意.
