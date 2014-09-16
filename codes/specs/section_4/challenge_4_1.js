b7f3be9d4b7d0de7072dea177b20b3e562a25775
Channely <qijie29896@gmail.com>
2014-09-16 12:50:13 +0800

    debug

--- codes/specs/section_4/challenge_4_1.js
+++ codes/specs/section_4/challenge_4_1.js
@@ -1,10 +1,10 @@
 describe('section_4', function () {
-    get_inputs("Running...",show);
+//    get_inputs("Running...",show);
     it("challenge_4_1", function () {
         get_inputs(flag,function(data){++data;})
         expect(logs[0]).toBe("name: bower");
         expect(logs[1]).toBe("speciality: Robot");
-        expect(logs[2]).toBe("Running...");
+//        expect(logs[2]).toBe("Running...");
         expect(info[0].name).toBe("bower");
         expect(info[0].speciality).toBe("Robot");
 
